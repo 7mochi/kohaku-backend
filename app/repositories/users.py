e43677a1ffb6fae5a2f5abc2a61286ea5976b96d
Alonso <33896771+7mochi@users.noreply.github.com>
2023-09-24 21:46:02 -0500

    Implement missing auth logic (#2)
    * feat: Implement missing auth logic
    * feat: Implement deauth endpoint

--- app/repositories/users.py
+++ app/repositories/users.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import cast
 from typing import TypedDict
+from uuid import UUID
 
 from common import clients
 from common.typing import UNSET
@@ -11,9 +12,14 @@ from common.typing import Unset
 READ_PARAMS = """
     user_id,
     discord_id,
-    username,
+    discord_username,
+    osu_id,
+    osu_username,
     verified,
     verification_code,
+    access_token,
+    refresh_token,
+    session_id,
     created_at,
     updated_at
 """
@@ -22,39 +28,60 @@ READ_PARAMS = """
 class User(TypedDict):
     user_id: int
     discord_id: str
-    username: str
+    discord_username: str
+    osu_id: str | None
+    osu_username: str | None
     verified: bool
     verification_code: str
+    access_token: str | None
+    refresh_token: str | None
+    session_id: UUID | None
     created_at: datetime
     updated_at: datetime
 
 
 class UserUpdateFields(TypedDict, total=False):
     discord_id: str
-    username: str
+    discord_username: str
+    osu_id: str
+    osu_username: str
     verified: bool
     verification_code: str
+    access_token: str | None
+    refresh_token: str | None
 
 
 async def create(
     discord_id: int,
-    username: str,
+    discord_username: str,
+    osu_id: int | None,
+    osu_username: str | None,
     verified: bool,
     verification_code: str,
+    access_token: str | None = None,
+    refresh_token: str | None = None,
+    session_id: UUID | None = None,
 ) -> User:
     user = await clients.database.fetch_one(
         query=f"""\
-            INSERT INTO users (discord_id, username, verified, verification_code,
-                               created_at, updated_at)
-            VALUES (:discord_id, :username, :verified, :verification_code,
-                    NOW(), NOW())
+            INSERT INTO users (discord_id, discord_username, osu_id, osu_username,
+                               verified, verification_code, access_token, refresh_token,
+                               session_id, created_at, updated_at)
+            VALUES (:discord_id, :discord_username, :osu_id, :osu_username,
+                    :verified, :verification_code,:access_token, :refresh_token,
+                    :session_id, NOW(), NOW())
             RETURNING {READ_PARAMS}
         """,
         values={
             "discord_id": str(discord_id),
-            "username": username,
+            "discord_username": discord_username,
+            "osu_id": str(osu_id),
+            "osu_username": osu_username,
             "verified": verified,
             "verification_code": verification_code,
+            "access_token": access_token,
+            "refresh_token": refresh_token,
+            "session_id": session_id,
         },
     )
 
@@ -91,7 +118,7 @@ async def fetch_by_user_id(user_id: int) -> User | None:
             WHERE user_id = :user_id
         """,
         values={
-            "user_id": user_id,
+            "user_id": str(user_id),
         },
     )
 
@@ -113,15 +140,15 @@ async def fetch_by_discord_id(discord_id: int) -> User | None:
     return cast(User, user) if user is not None else None
 
 
-async def fetch_by_username(username: int) -> User | None:
+async def fetch_by_discord_username(discord_username: int) -> User | None:
     user = await clients.database.fetch_one(
         query=f"""\
             SELECT {READ_PARAMS}
             FROM users
-            WHERE username = :username
+            WHERE discord_username = :discord_username
         """,
         values={
-            "username": username,
+            "discord_username": discord_username,
         },
     )
 
@@ -145,20 +172,35 @@ async def fetch_by_verification_code(verification_code: str) -> User | None:
 
 async def partial_update(
     user_id: int,
-    discord_id: str | Unset = UNSET,
-    username: str | Unset = UNSET,
+    discord_id: int | Unset = UNSET,
+    discord_username: str | Unset = UNSET,
+    osu_id: str | Unset = UNSET,
+    osu_username: str | Unset = UNSET,
     verified: bool | Unset = UNSET,
     verification_code: str | Unset = UNSET,
+    access_token: str | Unset = UNSET,
+    refresh_token: str | Unset = UNSET,
+    session_id: UUID | Unset = UNSET,
 ) -> User | None:
     update_fields = UserUpdateFields = {}
     if not isinstance(discord_id, Unset):
-        update_fields["discord_id"] = discord_id
-    if not isinstance(username, Unset):
-        update_fields["username"] = username
+        update_fields["discord_id"] = str(discord_id)
+    if not isinstance(discord_username, Unset):
+        update_fields["discord_username"] = discord_username
+    if not isinstance(osu_id, Unset):
+        update_fields["osu_id"] = str(osu_id)
+    if not isinstance(osu_username, Unset):
+        update_fields["osu_username"] = osu_username
     if not isinstance(verified, Unset):
         update_fields["verified"] = verified
     if not isinstance(verification_code, Unset):
         update_fields["verification_code"] = verification_code
+    if not isinstance(access_token, Unset):
+        update_fields["access_token"] = access_token
+    if not isinstance(refresh_token, Unset):
+        update_fields["refresh_token"] = refresh_token
+    if not isinstance(session_id, Unset):
+        update_fields["session_id"] = session_id
 
     query = f"""\
         UPDATE users
--- database/base.sql
+++ database/base.sql
@@ -1,9 +1,14 @@
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     discord_id TEXT NOT NULL,
-    username TEXT NOT NULL,
+    discord_username TEXT NOT NULL,
+    osu_id TEXT NULL,
+    osu_username TEXT NULL,
     verified BOOLEAN NOT NULL DEFAULT FALSE,
     verification_code TEXT NOT NULL,
+    access_token TEXT NULL,
+    refresh_token TEXT NULL,
+    session_id UUID NULL,
     created_at TIMESTAMPTZ NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL
 );
