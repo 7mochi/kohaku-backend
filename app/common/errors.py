6816c262e85aef241ac5b106f6dbf0a24d1ecf90
7mochi <flyingcatdm@gmail.com>
2023-09-30 18:32:51 -0500

    refact: use client storage instead of handling ourselves

--- app/common/errors.py
+++ app/common/errors.py
@@ -7,4 +7,3 @@ class ServiceError(str, Enum):
     INTERNAL_SERVER_ERROR = "global.internal_server_error"
 
     USER_NOT_FOUND = "user.not_found"
-    USER_ALREADY_VERIFIED = "user.already_verified"
